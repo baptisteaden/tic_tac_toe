5372d245d798d6d501081446f59acf43ca058326
Baptiste Aden <baptiste.aden@viavisolutions.com>
2020-12-30 12:40:03 +0100

    begin to add reactivity + game over logic

--- main.cpp
+++ main.cpp
@@ -3,8 +3,37 @@
 
 const int BOARD_SIZE = 3;
 const std::string ROW_SEPARATOR = "\n---+---+---\n";
+struct coords {
+    int x;
+    int y;
+} playCoords;
+int play = -1;
+char player = 'X';
+bool gameOver = false;
+std::string cells[BOARD_SIZE][BOARD_SIZE] = {
+        {" ", " ", " "},
+        {" ", " ", " "},
+        {" ", " ", " "}};
 
-void DisplayHelloMessage()
+bool IsDiagPlay(coords playCoords) {
+    bool ret = false;
+
+    // LTR diag
+    if(playCoords.x == playCoords.y) {
+        return true;
+    }
+
+    // RTL diag
+    for(int i = 0; i < BOARD_SIZE; i++) {
+        for(int j = BOARD_SIZE; j >= 0; j--) {
+            if(playCoords.x == i && playCoords.y == j) {
+                return true;
+            }
+        }
+    }
+}
+
+void Greet()
 {
     system("clear");
     std::cout << "\n--- YEAH TIC TAC TOE GAME ---\n\n";
@@ -15,7 +44,7 @@ void DisplayHelloMessage()
     std::cout << "Have fun!\n\n";
 }
 
-void Render(std::string cells[BOARD_SIZE][BOARD_SIZE])
+void Render()
 {
     std::string row = "";
     std::string render = "";
@@ -35,33 +64,119 @@ void Render(std::string cells[BOARD_SIZE][BOARD_SIZE])
     std::cout << render << "\n\n";
 }
 
-int Play(char player)
+int Play()
 {
-    int cellChoice;
-
     std::cout << "Player " << player << " is playing...\n";
-
-    std::cin >> cellChoice;
+    std::cin >> play;
     //TODO: check 1<=int<=9
     //if ()
-    return cellChoice;
+}
+
+void Update()
+{
+    // Translate play into (x,y) coords
+    switch(play) {
+       case 7: {
+           playCoords.x = 0;
+           playCoords.y = 0;
+           break;
+       }
+       case 8: {
+           playCoords.x = 0;
+           playCoords.y = 1;
+           break;
+       }
+       case 9: {
+           playCoords.x = 0;
+           playCoords.y = 2;
+           break;
+       }
+       case 4: {
+           playCoords.x = 1;
+           playCoords.y = 0;
+           break;
+       }
+       case 5: {
+           playCoords.x = 1;
+           playCoords.y = 1;
+           break;
+       }
+       case 6: {
+           playCoords.x = 1;
+           playCoords.y = 2;
+           break;
+       }
+       case 1: {
+           playCoords.x = 2;
+           playCoords.y = 0;
+           break;
+       }
+       case 2: {
+           playCoords.x = 2;
+           playCoords.y = 1;
+           break;
+       }
+       case 3: {
+           playCoords.x = 2;
+           playCoords.y = 2;
+           break;
+       }
+    }
+
+    // Update 
+    cells[playCoords.x][playCoords.y] = player;
+    player = (player == 'X') ? 'O' : 'X';
+}
+
+bool GameOver() 
+{
+    // Row
+    std::string row = "";
+    for (int i = 0; i < BOARD_SIZE && !gameOver; i++)
+    {
+        row += cells[playCoords.x][i];
+    }
+    if(row == "XXX" || row == "OOO") return true;
+
+    // Col
+    std::string col = "";
+    for (int i = 0; i < BOARD_SIZE && !gameOver; i++)
+    {
+        col += cells[i][playCoords.y];
+    }
+    if(col == "XXX" || col == "OOO") return true;
+
+    // LTR diag
+    std::string ltrDiag = "";
+    for(int i = 0; i < BOARD_SIZE; i++) {
+        for(int j = 0; j < BOARD_SIZE; j++) {
+            ltrDiag += cells[i][j];
+        }
+    }
+    if(ltrDiag == "XXX" || ltrDiag == "OOO") return true;
+
+    // RTL diag
+    std::string rtlDiag = "";
+    for(int i = 0; i < BOARD_SIZE; i++) {
+        for(int j = BOARD_SIZE; j >= 0; j--) {
+            rtlDiag += cells[i][j];
+        }
+    }
+    if(rtlDiag == "XXX" || rtlDiag == "OOO") return true;
+
+    return false;
 }
 
 int main()
 {
-    std::string cells[BOARD_SIZE][BOARD_SIZE] = {
-        {" ", " ", " "},
-        {" ", " ", " "},
-        {" ", " ", " "}};
-    int cellChoice;
-    char player = 'X';
-
-    // while(true){
-    DisplayHelloMessage();
-    Render(cells);
-    Play(player);
-    // //Update();
-    // }
+    gameOver = false;
+    while(!gameOver){
+        Greet();
+        Render();
+        Play();
+        Update();
+        gameOver = GameOver();
+    }
 
     return 0;
 }
